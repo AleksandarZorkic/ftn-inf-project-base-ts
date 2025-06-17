c573869304acd37116adb75d2f0c2809df1e078a
Aleksandar Zorkic <aleksandarzorkic@gmail.com>
2025-06-17 19:27:38 +0200

    Napravljene HTML i SASS datoteke, TypeScript datoteke za stranicu, servis i model podataka za korisnika i izolovan je u zasebnoj datoteci.

---
+++ app/models/usersFormData.models.ts
@@ -0,0 +1,6 @@
+export interface usersFormData {
+    username: string
+    name: string
+    surname: string
+    dateOfBirth: Date
+}
---
+++ app/service/userService.ts
@@ -0,0 +1,8 @@
+export class userService {
+    private apiUlr: string;
+
+    constructor() {
+        this.apiUlr = 'http://localhost:61486/api/users'
+    }
+
+}
